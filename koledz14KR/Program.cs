8151158e7cb97c972522462035be9b1dd520d552
Geord <Geord@DESKTOP-G489K85>
2025-10-21 09:55:06 +0300

    Добавьте файлы проекта.

---
+++ koledz14KR/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace koledz14KR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int vaga;
+            //float rost, indexMasiTila = 0;
+            //Console.Write("Вага: ");
+            //vaga = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Ріст: ");
+            //rost = Convert.ToSingle(Console.ReadLine());
+            //if (vaga > 0 && rost > 0)
+            //{
+            //    indexMasiTila = vaga / ((rost / 100) * (rost / 100));
+            //    Console.WriteLine("Індекс маси тіла: " + indexMasiTila);
+            //    if (indexMasiTila < 18.5)
+            //    {
+            //        Console.WriteLine("Недостатня вага");
+            //    }
+            //    else if (indexMasiTila >= 18.5 && indexMasiTila < 24.9)
+            //    {
+            //        Console.WriteLine("Норма");
+            //    }
+            //    else if (indexMasiTila >= 25 && indexMasiTila < 29.9)
+            //    {
+            //        Console.WriteLine("Надлишкова вага");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Ожиріння");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Введені некоректні дані.");
+            //}
+
+
+
+            //int visota;
+            //Console.Write("Введіть висоту піраміди: ");
+            //visota = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= visota; i++)
+            //{
+            //    for (int j = visota; j > i; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write(k);
+            //    }
+            //    for (int l = i - 1; l >= 1; l--)
+            //    {
+            //        Console.Write(l);
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
